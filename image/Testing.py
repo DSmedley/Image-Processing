e24755d287d768c08cf3c089151cc73bf773d021
Youmin Zheng <zheng.youmin@gmail.com>
2020-03-08 21:12:13 -0400

    restructured where the images are

--- image/Testing.py
+++ image/Testing.py
@@ -10,12 +10,12 @@ def text_detect(input, ele_size=(8, 2)):
         print("Equals 3")
         # Turn to black and white
         test = cv2.cvtColor(input, cv2.COLOR_BGR2GRAY)
-        cv2.imwrite("1.png", test)
+        cv2.imwrite("image_samples/1.png", test)
     # inverts colors
     img_sobel = cv2.Scharr(test, cv2.CV_8U, 1, 0)
-    img_sobel2 = cv2.Sobel(test, cv2.CV_8U, 0, 1)
-    cv2.imwrite("2.png", img_sobel)
-    cv2.imwrite("3.png", img_sobel2)
+    #img_sobel2 = cv2.Sobel(test, cv2.CV_8U, 0, 1)
+    #cv2.imwrite("image_samples/2.png", img_sobel)
+    #cv2.imwrite("image_samples/3.png", img_sobel2)
     img_threshold = cv2.threshold(img_sobel, 0, 255, cv2.THRESH_OTSU + cv2.THRESH_BINARY)
     print(img_threshold)
     element = cv2.getStructuringElement(cv2.MORPH_RECT, ele_size)
@@ -32,7 +32,7 @@ def text_detect(input, ele_size=(8, 2)):
 
 
 def main(input_file):
-    outputFile = input_file.split('.')[0] + '-rect.' + '.'.join(input_file.split('.')[1:])
+    outputFile = input_file.split('.')[0] + '-detected.' + '.'.join(input_file.split('.')[1:])
     print(outputFile)
     img = io.imread(input_file)
     rect = text_detect(img)
@@ -46,4 +46,4 @@ def main(input_file):
 
 
 if __name__ == '__main__':
-    main("ML-ImageTest.png")
+    main("image_samples/step1_image_colored.png")
