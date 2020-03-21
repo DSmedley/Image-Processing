a534c507472cc7c56858c7ecade53391809aea78
Dillon <smedleydillon@gmail.com>
2020-03-21 16:56:17 -0400

    Dont allow text to overlap on the image

--- image/generate_image_dataset.py
+++ image/generate_image_dataset.py
@@ -7,56 +7,77 @@ import os
 # TODO: Get the height and width of the image = DONE
 # TODO: Choose said number of words from random words to put onto the image = DONE
 # TODO: Randomly generate a coordinate for where the words will go = DONE
-# TODO: Randomly generate a number for font size
-# TODO: Choose random font
-# TODO: Apply the words onto the image
+# TODO: Randomly generate a number for font size = DONE
+# TODO: Choose random font = DONE
+# TODO: Apply the words onto the image = DONE
 # TODO: Log the name of the file, text, and text location onto a json object
+# TODO: Error handle when random words fail
+
+
+def dont_overlap():
+    something = True
+    for current_word in no_text_allowed:
+        old_bottom = current_word[0]
+        old_left = current_word[1]
+        old_top = current_word[2]
+        old_right = current_word[3]
+
+        # Based on new word
+        new_bottom_higher_old_top = new_bottom < old_top
+        new_top_lower_old_bottom = new_top > old_bottom
+        new_left_right_old_right = new_left > old_right
+        new_right_left_old_left = new_right < old_left
+        if not (new_bottom_higher_old_top or
+                new_top_lower_old_bottom or
+                new_left_right_old_right or
+                new_right_left_old_left):
+            something = False
+    return something
+
+
+def get_random_words():
+    r = RandomWords()
+    return r.get_random_words()
+
+
+def get_image_attributes(file):
+    original_img = cv2.imread('unclassified_images/' + file)
+    original_height = original_img.shape[0]
+    original_width = original_img.shape[1]
+    return original_img, original_height, original_width
 
 
 if __name__ == "__main__":
     # Loop through the directory using each image
     for filename in os.listdir('unclassified_images'):
-        r = RandomWords()
-        number_of_words = randrange(5)
-        random_words = r.get_random_words()
-
-        img = cv2.imread('unclassified_images/' + filename)
-
-        height = img.shape[0]
-        width = img.shape[1]
-
+        random_words = get_random_words()
+        img, height, width = get_image_attributes(filename)
         no_text_allowed = []
 
-        for i in range(5):
-            # TODO: make sure the coordinates dont overlap so words dont overlap
+        for i in range(randrange(15)):
+            # TODO: make sure the coordinates dont overlap so words dont overlap = DONE
             word = random_words[randrange(49)]
-
             font = randrange(7)
             thickness = 2
-            #TODO: Randomize these parameters
-            fontScale = 1
+            # TODO: Randomize these parameters
+            font_scale = randrange(1, 2)
             color = (0, 0, 0)
 
             # Find width and height of the word to make sure the whole word in on the image
-            (label_width, label_height), baseline = cv2.getTextSize(word, font, fontScale, thickness)
-
+            (label_width, label_height), baseline = cv2.getTextSize(word, font, font_scale, thickness)
             while True:
-                random_height = randrange(height - label_height - baseline)
-                random_width = randrange(width - label_width)
+                new_bottom = randrange(label_height + baseline, height)
+                new_left = randrange(width - label_width)
+                new_top = new_bottom - label_height - baseline
+                new_right = new_left + label_width
 
-                print(no_text_allowed)
                 if len(no_text_allowed) == 0:
                     break
-
-                for i in no_text_allowed:
-                    if((random_height < i[0] or random_height > i[2]) and (random_width < i[1] or random_width > i[3])):
-                        break
-
-            org = (random_width, random_height)
-
-            no_text_allowed.append([random_height, random_width, label_height+baseline+random_height, random_width+label_width])
-
-            image = cv2.putText(img, word, org, font, fontScale, color, thickness, cv2.LINE_AA)
+                if dont_overlap():
+                    break
+            org = (new_left, new_bottom)
+            no_text_allowed.append([new_bottom, new_left, new_top, new_right])
+            image = cv2.putText(img, word, org, font, font_scale, color, thickness, cv2.LINE_AA)
 
         cv2.imshow('image', image)
         cv2.waitKey(0)
